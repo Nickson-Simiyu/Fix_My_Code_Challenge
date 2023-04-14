41540654a3eee98ad48c9df8dadefa13d793420b
Nickson-Simiyu <simiyunickson1@gmail.com>
2023-04-14 04:35:12 -0700

    perfect square

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,27 +1,23 @@
 #!/usr/bin/python3
 
-class square():
-    
-    width = 0
-    height = 0
-
-
 class Square:
-
+    
     def __init__(self, width=0, height=0):
+        if width < 0 or height < 0:
+            raise ValueError("Width and height must be positive values.")
         self.width = width
         self.height = height
 
     def area(self):
         """Calculate the area of the square."""
-        return self.width * self.width
+        return self.width * self.height
 
     def perimeter(self):
         """Calculate the perimeter of the square."""
         return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
-        return "{}x{}".format(self.width, self.height)
+        return "Square(width={}, height={})".format(self.width, self.height)
 
 if __name__ == "__main__":
     s = Square(width=12, height=9)
